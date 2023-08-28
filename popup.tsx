4ca52fb8f1abbc8afe900e25b46e0f8b0fe0d3ab
unknown <hetarth002@gmail.com>
2023-08-28 20:09:23 +0530

    get user ip address

--- popup.tsx
+++ popup.tsx
@@ -1,29 +1,21 @@
-import { useState } from "react"
+import axios from "axios"
+import { useReducer, useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [count, increase] = useReducer((c) => c + 1, 0)
+  const [ipAddress, setIpAddress] = useState("")
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
-  )
+  const handleClick = async (e) => {
+    try {
+      e.preventDefault()
+      const response = await axios.get("https://api.ipify.org?format=json")
+      setIpAddress(response.data.ip)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return <button onClick={handleClick}>Get {ipAddress}</button>
 }
 
 export default IndexPopup
