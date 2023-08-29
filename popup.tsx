a92f9572e34886a9a00ccb2ef84f8ba0487c0fd9
unknown <hetarth002@gmail.com>
2023-08-29 23:08:23 +0530

    display city & country

--- popup.tsx
+++ popup.tsx
@@ -1,19 +1,30 @@
 import axios from "axios"
-import { useReducer, useState } from "react"
+import { useEffect, useState } from "react"
+
+const apiKey = process.env.REACT_APP_API_KEY
 
 function IndexPopup() {
   const [ipAddress, setIpAddress] = useState("")
-
-  const handleClick = async (e) => {
-    try {
-      e.preventDefault()
-      const response = await axios.get("https://api.ipify.org?format=json")
-      setIpAddress(response.data.ip)
-    } catch (err) {
-      console.log(err)
-    }
+  const [city, setCity] = useState("")
+  const [country, setCountry] = useState("")
+  const [text, setText] = useState("Get my location")
+  useEffect(() => {
+    axios
+      .get("https://api.ipify.org?format=json")
+      .then((res) => setIpAddress(res.data.ip))
+      .catch((err) => console.log(err))
+    axios
+      .get(`https://ipinfo.io/${ipAddress}?token=bc8e873c16773f`)
+      .then((res) => {
+        setCity(res.data.city)
+        setCountry(res.data.country)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+  const handleClick = () => {
+    setText(`Your country is ${country} and city is ${city}`)
   }
-  return <button onClick={handleClick}>{ipAddress}</button>
+  return <button onClick={handleClick}>{text}</button>
 }
 
 export default IndexPopup
