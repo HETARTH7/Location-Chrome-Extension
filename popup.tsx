f6b0c07b46cde33e3a66486b1802435cb219157a
unknown <hetarth002@gmail.com>
2023-08-30 11:21:13 +0530

    add errors

--- popup.tsx
+++ popup.tsx
@@ -3,33 +3,50 @@ import { useEffect, useState } from "react"
 
 import "./styles.css"
 
-const apiKey = process.env.REACT_APP_API_KEY
+const apiKey = process.env.REACT_APP_API_KEY || "bc8e873c16773f"
 
 function IndexPopup() {
   const [ipAddress, setIpAddress] = useState("")
   const [city, setCity] = useState("")
   const [country, setCountry] = useState("")
   const [text, setText] = useState("Get my location")
+  const [error, setError] = useState(null) // Add state for error
+
   useEffect(() => {
     axios
       .get("https://api.ipify.org?format=json")
-      .then((res) => setIpAddress(res.data.ip))
-      .catch((err) => console.log(err))
-    axios
-      .get(`https://ipinfo.io/${ipAddress}?token=bc8e873c16773f`)
       .then((res) => {
-        setCity(res.data.city)
-        setCountry(res.data.country)
+        setIpAddress(res.data.ip)
+        axios
+          .get(`https://ipinfo.io/${res.data.ip}?token=${apiKey}`)
+          .then((res) => {
+            setCity(res.data.city)
+            setCountry(res.data.country)
+          })
+          .catch((err) => {
+            setError("Error fetching location data.")
+            console.log(err)
+          })
       })
-      .catch((err) => console.log(err))
+      .catch((err) => {
+        setError("Error fetching IP address.")
+        console.log(err)
+      })
   }, [])
+
   const handleClick = () => {
     setText(`Your country is ${country} and city is ${city}`)
   }
+
   return (
-    <button className="bg-sky-500 w-96 h-96 text-white" onClick={handleClick}>
-      {text}
-    </button>
+    <div>
+      {error && <p className="text-red-500">{error}</p>}
+      <button
+        className="bg-sky-500 w-96 h-96 text-white text-2xl"
+        onClick={handleClick}>
+        {text}
+      </button>
+    </div>
   )
 }
 
